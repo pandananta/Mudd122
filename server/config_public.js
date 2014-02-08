89bae5433b1f406d5806fc677d2fad61b66da984
Rachel <rs3092@barnard.edu>
2014-02-08 02:17:10 -0500

    clean-up

--- server/config_public.js
+++
@@ -1,9 +0,0 @@
-Accounts.loginServiceConfiguration.remove({
-	service: "google"
-})
-
-Accounts.loginServiceConfiguration.insert({
-	service: "google",
-	clientId: "client ID here",
-	secret: "client secret here"
-})
